8d18a61a25090e9f5f9ef2a868cc3c16b4f97d41
Derrick Liu <derrick56007@hotmail.com>
2019-02-28 09:59:14 -0800

    commit

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,16 @@
 function initMap() {
-	// add your code here
+    // add your code here
+
+    L.mapquest.key = 'QKJgWM50iUAB7L2HitjGtmLCdDbXEavf';
+
+    var map = L.mapquest.map('map', {
+        center: [32.878797, -117.235918],
+        center: [32.8788, -117.2359],
+        layers: L.mapquest.tileLayer('map'),
+        zoom: 12,
+        zoom: 14,
+    });
+
+
+    L.marker([32.88, -117.237]).addTo(map);
 }
